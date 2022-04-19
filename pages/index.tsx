5a7ac7267ec645796980dbcb1b997182e8ce5c9e
turboBarbitura <turbobarbitura@mail.ru>
2022-04-19 07:05:03 +0700

    pixel bug fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import {GiBrassKnuckles} from 'react-icons/gi'
+import { GiBrassKnuckles } from 'react-icons/gi'
 import knuckles from '../public/knuckles-red.png'
 
 const Home: NextPage = () => {
@@ -11,24 +11,21 @@ const Home: NextPage = () => {
       <Head>
         <title>turboBarbitura</title>
         <meta name="description" content="street art person" />
-        <link rel="icon" href='brass-knuckles.png' />
+        <link rel="icon" href="brass-knuckles.png" />
       </Head>
-      
-      
+
       <div className={styles.main}>
         <div>
-          <h1 className={styles.logo1}>Добро пожаловать в</h1>
+          <h1 className={styles.logo}>Добро пожаловать в Но &nbsp;&nbsp; осибирск.</h1>
         </div>
-         <div className={styles.secondItem}>
-            <h1 className={styles.logo2}>Но</h1>
-            <div className={styles.knuckle}>
-             <Image className={styles.brassKnuckle} src={knuckles} alt='knuckles'/>
-            </div>
-            <h1 className={styles.logo3}>осибирск.</h1>
-         </div>
+        <div className={styles.knuckle}>
+          <Image
+            className={styles.brassKnuckle}
+            src={knuckles}
+            alt="knuckles"
+          />
+        </div>
       </div>
-
-       
 
       {/* <GiBrassKnuckles className={styles.brassKnuckle}/> */}
     </div>
