cf28407de8ea9ecec81607c24164873ce607db0b
turboBarbitura <turbobarbitura@mail.ru>
2022-07-14 17:53:50 +0700

    header update

--- components/Header/header.tsx
+++ components/Header/header.tsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import Link from "next/link";
+
+import styles from './Header.module.scss'
+
 
 const Header = () => {
-  return (
-    <div>header</div>
-  )
+    return (
+
+        <div className={styles.header}>
+            <div className={styles.wrapper}>
+                <Link href='/'>
+                <h1 className={styles.logo}>
+                    turboBarbitura
+                </h1>
+                </Link>
+                <nav>
+                    <ul className={styles.navItems}>
+                        <Link href='/cv'><li>Резюме</li></Link>
+                        <Link href='/about'><li>Об авторе</li></Link>
+
+                    </ul>
+
+                </nav>
+            </div>
+        </div>
+
+    )
 }
 
 export default Header
--- pages/cv.tsx
+++ pages/cv.tsx
@@ -1,115 +1,28 @@
-import { NextPage } from 'next'
-import Image from 'next/image'
-import React from 'react'
-import styles from '../styles/Cv.module.css'
-import Avatar from '../public/cv-avatar.jpg'
-import { AiFillHome, AiFillPhone, AiFillMail, AiFillGithub } from 'react-icons/ai';
-import { FaTelegram } from 'react-icons/fa';
-
+import {NextPage} from 'next';
+import React from 'react';
+import styles from '../styles/Cv.module.scss';
+import CvSideBar from '../components/CV/CvSideBar';
+import CvBody from '../components/CV/CvBody';
+import Head from "next/head";
 
 const Cv: NextPage = () => {
-  return (
-    <div className={styles.container}>
-      <div className={styles.contentWrapper}>
-        <div className={styles.sidebar}>
-          <Image
-            className={styles.avatar}
-            src={Avatar}
-            alt="avatar"
-            layout="responsive"
-          />
-          <h1>Михаил Щукин</h1>
-          <h3>Фронтенд разработчик</h3>
+    console.log(new Date().getFullYear());
 
-          <div className={styles.contacts}>
-
-
-            {/* АДРЕС */}
-            <div className={styles.contact}>
-              <a href='https://yandex.ru/maps/-/CCUFYUtbgB' rel='noreferrer' 
-                target='_blank'>
-                <AiFillHome className={styles.icon}/> 
-              </a>
-              <a className={styles.link}  href='https://yandex.ru/maps/-/CCUFYUtbgB'>Новосибирск</a>
+    return (
+        <>
+            <Head>
+                <title>Мое резюме</title>
+                <meta name="description" content="Резюме на вакансию фрнотенд дев"/>
+                <link rel="icon" href="brass-knuckles.png"/>
+            </Head>
+            <div className={styles.container}>
+                <div className={styles.contentWrapper}>
+                    <CvSideBar/>
+                    <CvBody/>
+                </div>
             </div>
+        </>
+    );
+};
 
-
-            {/* ТЕЛЕФОН */}
-            <div className={styles.contact}>
-              <a href='tel:+79231145870'>
-                <AiFillPhone className={styles.icon}/> 
-              </a>
-              <a className={styles.link} href='tel:+79231145870'>+7-923-114-58-70</a>
-            </div>
-
-
-            {/* ПОЧТА */}
-            <div className={styles.contact}>
-              <a href='mailto:mikhialxshchukin@gmail.com'>
-                <AiFillMail className={styles.icon}/> 
-              </a>
-              <a className={styles.link} href='mailto:mikhialxshchukin@gmail.com'>mikhialxshchukin@gmail.com</a>
-            </div>
-
-
-            {/* ТЕЛЕГА */}
-            <div className={styles.contact}>
-              <a 
-                href="https://t.me/turboBarbitura" 
-                rel='noreferrer' 
-                target='_blank'>
-                <FaTelegram className={styles.icon}/> 
-              </a>
-              <a 
-                className={styles.link} 
-                href="https://t.me/turboBarbitura" 
-                rel='noreferrer' 
-                target='_blank'>
-                  t.me/turboBarbitura
-              </a>
-            </div>
-
-
-            {/* ГИТХАБ */}
-            <div className={styles.contact}>
-              <a 
-                href="https://github.com/turboBarbitura" 
-                rel='noreferrer' 
-                target='_blank'>
-                <AiFillGithub className={styles.icon}/> 
-              </a>
-              <a 
-                className={styles.link} 
-                href="https://github.com/turboBarbitura" 
-                rel='noreferrer' 
-                target='_blank'>
-                  github.com/turboBarbitura
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className={styles.content}>
-        HTML/CSS
-
-Responsive Design
-
-Bootstrap / Tailwind
-
-JavaScript / ES6+
-
-TypeScript
-
-React / Redux
-
-Redux Saga Toolkit
-
-Next JS
-
-Node JS
-        </div>
-      </div>
-    </div>
-  )
-}
-
-export default Cv
+export default Cv;
